6cc570f496cc87dc6b547699e8ff7828a3075611
Stevesuk0 <stevesukawa@outlook.com>
2025-01-16 00:34:48 +0800

    feature: copy & paste and support pipe etc boom shakalaka

---
+++ main.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import sys
+
+filename = 'dash.clips'
+
+def _clean():
+    with open(filename, 'w') as f:
+        f.write('')
+
+def _write(str):
+    with open(filename, 'a') as f:
+        f.write(str)
+
+def _read():
+    with open(filename, 'r') as f:
+        return f.read()
+
+if sys.argv[-1] in ('c', 'copy'):
+    _clean()
+    if sys.stdin.isatty():
+        print("Type anything you want to copy. Press ^C to confirm.")
+        try:
+            while True:
+                _write(input() + '\n')
+        except KeyboardInterrupt:
+            print("\nCopying finished.")
+    else:
+        #write(input())
+        for line in sys.stdin:
+            print(line, end='')
+            _write(line)
+
+else:
+    print(_read())
